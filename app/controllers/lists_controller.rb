494e2cb2515facee6d6bf9e999b8fbfac0833a62
AMAI-nayotake <194278002+AMAI>
2025-03-04 07:33:18 +0000

    [Add] app1-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,8 +18,9 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
