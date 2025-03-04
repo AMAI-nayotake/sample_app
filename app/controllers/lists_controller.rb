2ec91b2988d08de3c9f4526284af03f5f672389d
AMAI-nayotake <194278002+AMAI>
2025-03-04 07:05:55 +0000

    [Finish] app1-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,8 +6,9 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
+
   def index
     @lists = List.all
   end
@@ -18,6 +19,7 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
   private
   def list_params
     params.require(:list).permit(:title, :body)
